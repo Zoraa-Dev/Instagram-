193f1042e7adfd3a7f42827cf7810b9c2bd37ee1
Zoraa Dev <132505748+Zoraa-Dev@users.noreply.github.com>
2025-08-26 00:17:43 +0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,6 +2,6 @@ import os
 from Temporary.Licensenya import ZoraaDev
 
 if __name__=="__main__":
-try: os.mkdir('OK') or os.mkdir('CP')
-except: pass
-os.system('clear'); ZoraaDev()
+  try: os.mkdir('OK') or os.mkdir('CP')
+    except: pass
+      os.system('clear'); ZoraaDev()
